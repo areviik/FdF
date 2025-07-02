5425d6aaaea8d974cff87f2820f3fee85017458a
areviik <vardanyanarev48@gmail.com>
2025-07-02 23:11:40 +0400

    Update color.c

--- srcs/color.c
+++ srcs/color.c
@@ -7,17 +7,17 @@ int set_color(int T, int R, int G, int B)
 
 int color_t(int trgb)
 {
-    return ((trgb << 24) & 0xFF);
+    return ((trgb >> 24) & 0xFF);
 }
 
 int color_r(int trgb)
 {
-    return ((trgb << 16) & 0xFF);
+    return ((trgb >> 16) & 0xFF);
 }
 
 int color_g(int trgb)
 {
-    return ((trgb << 8) & 0xFF);
+    return ((trgb >> 8) & 0xFF);
 }
 
 int color_b(int trgb)
